646c0078ab3662485aaab98907dd7a18a82cb113
555 <Shebelnitskiy@gmail.com>
2023-05-19 12:58:18 +0400

    fix utls and views

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,10 +1,11 @@
 from  django.urls import path
-from catalog.views import home, contacts
+from catalog.views import home, contacts, thank
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 app_name = 'catalog'
 
 urlpatterns = [
     path('', home, name = 'home'),
-    path('contacts/', contacts, name = 'contacts')
+    path('contacts/', contacts, name = 'contacts'),
+    path('thank_you/', thank, name = 'thank')
     ] + staticfiles_urlpatterns()
--- catalog/views.py
+++ catalog/views.py
@@ -4,4 +4,14 @@ def home(request):
     return render(request, 'main/home.html')
 
 def contacts(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        phone = request.POST.get('phone')
+        message = request.POST.get('message')
+        print(f'{name}({phone}):{message}')
+        return render(request, 'main/thank_you.html')
     return render(request, 'main/contacts.html')
+
+def thank(request):
+    return render(request, 'main/thank_you.html')
+
